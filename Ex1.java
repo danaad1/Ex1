a9ca6e6b9c5aae1516ff45e24f799c348af65169
laptop1 <danaadler5@gmail.com>
2024-12-08 21:46:45 +0200

    added number number2Int

--- Ex1.java
+++ Ex1.java
@@ -1,3 +1,5 @@
+import org.apiguardian.api.API;
+
 /**
  * This class represents a simple solution for Ex1.
  * As defined here: https://docs.google.com/document/d/1AJ9wtnL1qdEs4DAKqBlO1bXCM6r6GJ_J/r/edit/edit
@@ -20,16 +22,41 @@ public class Ex1 {
         public static int number2Int(String num) {
             int ans = -1;
 
+            if (isNumber(num)){
+                ans = 0; // int representation of num
+                String a = "0123456789ABCDEFG"; // all possible characters for num
+                String number = num.split("b")[0]; // find the number part of num
+                int base = Integer.parseInt(num.split("b")[1]); // find the basis part of num and convert to int
+                int weight = 0 ; // weight of number to know by how much to multiply
 
+                for (int i = number.length() - 1; i >= 0; i-- ) {
+//                  for (int j = 0; j < a.length(); j++) {
+                    int index = a.indexOf(number.charAt(i)) ;
+                    ans += (int) (index * Math.pow(base,weight));
+                    weight += 1;
+//                  }
+                }
+            }
 
+//            String [] arr = num.split("b");
+//            int number = Integer.parseInt(arr[0]);
+//            int basis = Integer.parseInt(arr[1]);
+//
+//            int stop = number ; // fix
+//            ans = 0 ;
 
-
-
-            // add your code here
-
-            ////////////////////
+//            while (remainder > 0){
+//                ans += remainder % 10 ;
+//                remainder = number / 10 ;
+//            }
+//            ans += remainder % 10;
+//
+//            // add your code here
+//
+//            ////////////////////
             return ans;
         }
+
         /**
          * This static function checks if the given String (g) is in a valid "number" format.
          * @param a a String representing a number
@@ -37,7 +64,6 @@ public class Ex1 {
          */
         public static boolean isNumber(String a) {
             boolean ans = true;
-
             if (!a.contains("b")){
                 a += "bA";
             }
@@ -50,11 +76,16 @@ public class Ex1 {
 
             for(int i = 0; i<arrNum[0].length(); i++) { // loop over array index 0
                 char t = arrNum[0].charAt(i);
-                int n = validNum.indexOf(t +"");
+                int n = validNum.indexOf(t +""); //will pass 32b3b5 ?  break down to num valid bais val
                  if(!validNum.contains(t+ "") || n >= validNum.indexOf(arrNum[1])){ // if any number is invalid or is bigger than basis return false
                     ans = false;
+                    break;
                 }
             }
+             if (!validNum.contains(arrNum[1]) || arrNum[1].length() > 2){ // if basis in invalid or has multiple numbers
+                return false;
+             }
+
 
             // add your code here
 
@@ -107,4 +138,58 @@ public class Ex1 {
             ////////////////////
             return ans;
         }
-}
+
+    /**
+     * this static function converts a number represented as int to an int at a given basis represented as int
+     * @param num
+     * @param base
+     * @
+     */
+      public static int convertToBasis (int num, int base) {
+            int ans = 0;
+
+            //implement here//
+
+            return ans;
+       }
+
+    /**
+     * this static function converts number form a string form to an integer
+     * @param num
+     * @return
+     */
+//       public static int stringToInt (String num) {
+//          int intRepresent = 0; // int representation of num
+//          String a = "0123456789ABCDEFG"; // all possible characters for num
+//          String number = num.split("b")[0]; // find the number part of num
+//          String base = num.split("b")[1]; // find the basis part of num
+//           int weight = 0 ; // weight of number to know by how much to multiply
+//
+//           for (int i = number.length() - 1; i >= 0; i-- ) {
+////              for (int j = 0; j < a.length(); j++) {
+//                  int index = a.indexOf(i+ "");
+//                  intRepresent += (int) (index * Math.pow(base,weight));
+////              }
+//          }
+
+
+
+//          String  [] number  =  num.split("b"); //to ints add val of g
+//          int intNum =  ;
+//
+//
+//          for (int i = 0; i < number[0].length(); i++) {
+//              for (int j = 0 ; j < a.length() ; j++ ) {
+//                  intNum += a.charAt(j) ;
+//              }
+//          }
+//          System.out.println(intNum);
+
+
+
+
+
+          //implement
+//          return intRepresent;
+       }
+
