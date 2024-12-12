46485dc121723b779e19e9542be0b682e81f0a53
laptop1 <danaadler5@gmail.com>
2024-12-12 22:59:23 +0200

    fixed main

--- src/Ex1Main.java
+++ src/Ex1Main.java
@@ -20,18 +20,20 @@ public class Ex1Main {
             num1 = sc.next();
 
                 if (!num1.equals("quit")) { //continue unless quit
+                    System.out.println("num1 = " + num1 + " is number: " + Ex1.isNumber(num1) + " , value: " + Ex1.number2Int(num1));
                     if (!Ex1.isNumber(num1)){ // if num2 is not valid ask for new input
+                        System.out.println("ERR: num1 is in the wrong format! (" + num1 + ")");
                         continue;
                     }
-                    System.out.println("num1 = " + num1 + " is number: " + Ex1.isNumber(num1) + " , value: " + Ex1.number2Int(num1));
 
                     System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
                     num2 = sc.next();
                     if (!num2.equals("quit")) {  //continue unless quit
+                        System.out.println("num2 = " + num2 + " is number: " + Ex1.isNumber(num2) + " , value: " + Ex1.number2Int(num2));
                         if (!Ex1.isNumber(num2)){ // if num2 is not valid ask for new input
+                            System.out.println("ERR: num2 is in the wrong format! (" + num2 + ")");
                             continue;
                         }
-                        System.out.println("num2 = " + num2 + " is number: " + Ex1.isNumber(num2) + " , value: " + Ex1.number2Int(num2));
 
                         System.out.println("Enter a base for output: (a number [2,16]");
                         int base = sc.nextInt();
