4ade24faa6f642e554f0b96b3457622f83a421cd
laptop1 <danaadler5@gmail.com>
2024-12-12 12:37:27 +0200

    test commit

--- Ex1Main.java
+++ Ex1Main.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
  * Intro2CS, Ex1 - very basic "main template"
  * Make sure your implementation of this main performs as the Ex1Sol.jar solution implement all needed functions.
  *
+ *
+ *
  */
 public class Ex1Main {
     public static void main(String[] args) {
